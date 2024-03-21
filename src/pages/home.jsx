6ac398894e8581f9c51a9fb8cb1abee86bbd1481
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-03-21 15:53:16 -0400

    create index page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,11 @@
-export function Home(){
-    return(
+import { Link } from "react-router-dom"
+
+export function Home() {
+    return (
         <div>
             <h1>home</h1>
+            <p>This is the home page.</p>
+            <Link to="/indexPage">Go to Index Page</Link>
         </div>
     )
 }
