144dc37f8570172cb3fff43e40675578e9bdb0f8
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-03-21 21:02:02 -0400

    hotfix conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
-<<<<<<< HEAD
 import {createBrowserRouter, RouterProvider, Routes, Route} from "react-router-dom"
 import { Home } from './pages/home';
 import NewItemPage from './pages/NewItemPage';
-=======
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { Home } from './pages/home';
 import { Index } from './pages/indexPage';
 import { ItemDetails } from './pages/itemDetails';
->>>>>>> main
+
 
 
 function App() {
@@ -17,9 +13,7 @@ function App() {
       path: "/",
       element: <Home />
     },
-<<<<<<< HEAD
     
-=======
     {
       path: "/indexPage",
       element: <Index />
@@ -29,7 +23,6 @@ function App() {
       element: <ItemDetails />
     },
 
->>>>>>> main
   ]);
 
   return (
