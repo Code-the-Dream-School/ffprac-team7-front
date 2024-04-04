e38328c6d5990af8c32a82ff233a6d96c099b255
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-04-04 19:36:44 -0400

    complete landing page appearance

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,16 +5,21 @@ import { Link } from "react-router-dom";
 
 function LandingPage() {
     return (
-        <div>
-            <Header pageTitle="Landing Page" />
-            <h1>Welcome to the Landing Page</h1>
-            <p>Search:</p>
-            <input type='text' disabled />
-            <button disabled>Submit</button>
-            <p>Overview of Features</p>
-            <p>Feature 1: TBD</p>
-            <p>Feature 2: TBD</p>
-            <Link to="/">Back to Home Page</Link>
+        <div >
+            <div>
+                <Header pageTitle="Landing Page" />
+            </div>
+            <div className="landingPageContainer">
+                <h1>Welcome to Stuff<span>Findr</span></h1>
+                <p>Your go-to solution for swiftly reuniting with your cherished belongings!
+                    Seamlessly report lost items and connect with a finder when a match is found nearby.
+                    StuffFindr is here to make the journey of reclaiming lost items hassle-free and all at your fingertips!</p>
+                <div className="landingPageBtns">
+                    <button className="logInBtn" disabled>Log In</button>
+                    <button className="signUpBtn" disabled>Sign Up</button>
+                </div>
+                <Link to="/">Back to Home Page</Link>
+            </div>
         </div>
     );
 }
