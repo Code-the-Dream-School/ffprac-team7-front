c72ea8c6b44c126d7962de1eec21377378cb8bec
Roy Mosby <ego.madking@gmail.com>
2024-04-03 20:47:59 -0400

    create ItemCard

---
+++ src/shared/ItemCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import placeHolder from "../assets/placeholder.svg"
+
+const ItemCard = ({photo, title, dateLostFound, type,description, location}) =>{
+return (
+<div className="reportedItemsContainer">
+   <div className="ItemCard">
+   <img className="itemPhoto" src={photo || placeHolder}/>
+   <h3>{title}</h3>
+   <p>{description}</p>
+   <p>Reported: {dateLostFound}</p>
+   <p>{type}</p>
+   <p>{location}</p>
+  </div>
+</div>
+)
+};
+
+export default ItemCard;
