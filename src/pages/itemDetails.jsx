2d4618da6525bf88f73fa2f1a955a1a73e9be3d5
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-05 15:50:22 -0400

    item info page

--- src/pages/itemDetails.jsx
+++ src/pages/itemDetails.jsx
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom"
+import Header from "../shared/header";
+import "../styles/itemDetails.css";
 
 export function ItemDetails() {
     return (
-        <div>
-            <h1>Item Details</h1>
-            <p>This page shows the item details</p>
-            <Link to="/">Back to Home Page</Link>
+        <div class='item_details_container'>
+            <Header pageTitle="Item Details"/>
+            
+            <img class='itemImage' src='360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg'/>
+            
+            <h4 class='itemName'>Item Name</h4>
+            
+            <h4 class="description" > Description </h4>
+            <textarea class="rect1" ></textarea>  {/* <-- text area ? */}
+            
+            <h4 class="date" > Date Found </h4>
+            <textarea class="rect2" placeholder="DD/MM/YYYY"> DD/MM/YYYY </textarea>   {/* <-- text area ? */}
+           
+            <h4 class="location" > Location </h4>
+            <textarea class="rect3" placeholder='Location' ></textarea>   {/* <-- text area ? */}
+            
+            <h4 class="status" > Status </h4>
+            <div class="lostFound" > Lost </div>   
+
+            <div class="claim" > Claim </div>   {/*button*/}
+        
+            <Link class='BackHome' to="/">Back to Home Page</Link>
         </div>
     )
 }
+
+
