938fe24aa6dd1e9573281edea5545a264616f4c6
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-04-04 20:53:20 -0400

    change font to poppins and adjust font sizes to fit screen

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,8 +12,8 @@ function LandingPage() {
             <div className="landingPageContainer">
                 <h1>Welcome to Stuff<span>Findr</span></h1>
                 <p>Your go-to solution for swiftly reuniting with your cherished belongings!
-                    Seamlessly report lost items and connect with a finder when a match is found nearby.
-                    StuffFindr is here to make the journey of reclaiming lost items hassle-free and all at your fingertips!</p>
+                    Seamlessly report lost items and connect with a finder when a match is found.
+                    StuffFindr is here to make the journey of reclaiming lost items easy and all at your fingertips!</p>
                 <div className="landingPageBtns">
                     <button className="logInBtn" disabled>Log In</button>
                     <button className="signUpBtn" disabled>Sign Up</button>
