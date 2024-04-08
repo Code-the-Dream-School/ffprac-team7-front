e1f30da63ef96853411e4cbb0c3f3b2b01d7f72a
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2024-04-08 17:21:15 -0500

    created hamburger menu component, pending to add more links and perfect styling

--- src/shared/header.jsx
+++ src/shared/header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import './header.css';
+import HamburgerMenu from "../assets/HamburgerMenu";
 
 const Header = ({pageTitle}) => {
     return(
+        
         <div className='header-container'>
-            <div className='menu-icon'>&#9776;</div> 
+             <div><HamburgerMenu/> </div>
             <h1 className='logo'>{pageTitle || "StuffFindr"}</h1>
-        </div>
+        </div> 
     )
 }
 
