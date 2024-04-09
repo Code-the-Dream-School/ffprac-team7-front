5be234d0518fbe0121cf314a3284743826781902
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-09 18:34:40 -0400

    made changes to better match figma design

--- src/pages/itemDetails.jsx
+++ src/pages/itemDetails.jsx
@@ -7,36 +7,36 @@ export function ItemDetails() {
     return (
         <div>
             <Header pageTitle="Item Details"/>
-            <div className='item_details_container'>
+            <div className='itemDetailsContainer'>
                 <img className='itemImage' src={placeHolder} />
             
                 <h2 className='itemName'>Item Name</h2>
             
-                <div className="description-container"> 
+                <div className="descriptionContainer"> 
                     <h3 className="description" > Description </h3>
                     <p className="descriptionBox" > </p>  
                 </div>
                 
-                <div className='date-Container'>
+                <div className='dateContainer'>
                     <h3 className="date" > Date Found </h3>
                     <p className="dateBox"> DD/MM/YYYY</p>  
                 </div>
 
-                <div className='location-Container'>
+                <div className='locationContainer'>
                     <h3 className="location" > Location </h3>
                     <p className="locationBox">Location</p>  
                 </div>
                 
-                <div className='status-Container'>
+                <div className='statusContainer'>
                     <h3 className="status" > Status </h3>
                     <p className="lostFound" > Lost </p>   
                 </div>
 
-                <div className='claim-Button-Container'>
+                <div className='claimButtonContainer'>
                     <button className='button'> Claim </button>
                 </div>
 
-                <Link className='backHome' to="/">Back to Home Page</Link>
+              
             </div>
         </div>
     )
