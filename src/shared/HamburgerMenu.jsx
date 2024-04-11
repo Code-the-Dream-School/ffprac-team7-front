881259b092386e4c8e05c6df333f9fed7774ac79
Roy Mosby <ego.madking@gmail.com>
2024-04-11 13:39:24 -0400

    toggle menu character + hotfix close menu on any click

--- src/shared/HamburgerMenu.jsx
+++ src/shared/HamburgerMenu.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/HamburgerMenu.css"
+
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
+const currentLocation = useLocation();
 
   const toggleMenu = () => {
     console.log("menu called")
     setIsOpen(!isOpen);
+    console.log(currentLocation.pathname)
   };
 
   return (
@@ -17,9 +20,9 @@ const HamburgerMenu = () => {
         className= "hamburger-icon"
         onClick={toggleMenu}
       >
-       &#9776; </div>
-
-      <ul className={`list ${isOpen ? 'open' : ''}`} >
+        {isOpen? <>&#x2715;</> : <>&#9776;</>}
+        </div>
+      <ul className={`list ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/reportedItems">Reported Items</Link></li>
         <li><Link to="/reportNewItem">Report an Item</Link></li>
