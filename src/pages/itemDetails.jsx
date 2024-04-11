565d8ff0431536d760a818124a2c3a2b2ba59804
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-11 13:27:16 -0400

    trying to pull

--- src/pages/itemDetails.jsx
+++ src/pages/itemDetails.jsx
@@ -8,7 +8,6 @@ export function ItemDetails() {
         <div>
             <Header pageTitle="Item Details"/>
             <div className='itemDetailsContainer'>
-                
                 <img className='itemImage' src={placeHolder} alt="Placeholder" />
             
                 <h2 className='itemName'>Item Name</h2>
@@ -16,9 +15,12 @@ export function ItemDetails() {
                 <h3 className="description" > Description </h3>
                 <p className="descriptionBox" > </p>  
                 
-                <p className='dateLocationStatus' > Date Found <span className="dateBox"> DD/MM/YYYY</span></p>
-                <p className='dateLocationStatus'> Location <span className="locationBox">Location Name</span></p>
-                <p className='dateLocationStatus'> Status <span className="lostFound">Lost</span></p>
+                <p className='dateLine'> Date Found <span className="dateBox"> DD/MM/YYYY</span></p>
+                
+                <p className='locationLine'> Location <span className="locationBox">Location Name</span></p>
+                
+                <p className='statusLine'> Status <span className="lostFound">Lost</span>
+</p>
 
                 <div className='claimButtonContainer'>
                     <button className='button'> Claim </button>
