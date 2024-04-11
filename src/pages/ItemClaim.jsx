07f4d9d1846f15c5dd9ac7e535055792aac7c2c3
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2024-04-11 17:47:37 -0500

    claim component, pending linking to itemdetails page

--- src/pages/ItemClaim.jsx
+++ src/pages/ItemClaim.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Header from "../shared/header"
+import { useState } from "react";
 
 
 
-const ItemClaim = () =>{
+
+const ItemClaim = ({item}) =>{
+
+const [proveOwnership, setProveOwnership] = useState("");
+
     return (
         <div>
-    
+            <Header pageTitle="Claim Item" />
+    <img src={item.photo}/>
+    <h2>{item.title}</h2>
+    <form>
+        <label htmlFor="proveOwnership">Provide some unique info about item proving ownership</label>
+        <textarea
+        id="claim"
+        value={proveOwnership}
+        onChange={(e)=>setProveOwnership(e.target.value)}
+
+        />
+        <button>Submit Claim</button>
+
+    </form>
 
         </div>
 
