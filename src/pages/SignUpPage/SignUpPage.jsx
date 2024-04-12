f977031b25dc81446a00959a001911559a3364c4
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-04-11 23:12:07 -0400

    complete styling for sign up page without local storage for image upload

---
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Header from "../../shared/header";
+import "../../styles/SignUpPage.css";
+import { Link } from "react-router-dom";
+import ImageUpload from "./imageUpload";
+
+
+
+function SignupPage() {
+    return (
+        <div>
+            <Header pageTitle="StuffFindr" />
+            <div className="signUpPageContainer">
+                <h2>Sign up</h2>
+                <ImageUpload />
+                <div className="inputContainer">
+                    <input className="inputName" type='text' placeholder='Name' disabled />
+                    <input className="inputUsername" type='text' placeholder='Username' disabled />
+                    <input className="inputNumber" type='text' placeholder='Contact Number' disabled />
+                    <input className="inputEmail" type='text' placeholder='Email' disabled />
+                    <input className="inputPassword" type='password' placeholder='Password' disabled />
+                </div>
+                <p>Already have an account? <Link to="/LoginPage">Log In</Link></p>
+                <button className="signUpBtn" disabled>Submit</button>
+            </div>
+        </div>
+    );
+}
+
+export default SignupPage;
