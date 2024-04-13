225470a013cbcaa8e648d2047ad4699818081430
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-13 10:29:14 -0400

    set up loginCSS loginContainer and header

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom"
+import Header from "../shared/header";
+import "../styles/LoginPage.css";
 
 function LoginPage() {
     return(
         <div>
-            <h2>Login</h2>
-            <input type='text' placeholder='username' disabled />
-            <input type='password' placeholder='password' disabled />
-            <button disabled>Submit</button>
+            <Header />
+            <div className="LoginPageContainer">
+                <h2>Welcome to StuffFindr</h2>
+                <input type='text' placeholder='username' disabled />
+                <input type='password' placeholder='password' disabled />
+                <button disabled>Submit</button>
+            </div>
         </div>
     );
 }
