53b31a332dfa355fccedee629b384ae3b7ebec0e
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2024-04-14 16:03:38 -0500

    linked items page to ItemDetails to display specific Item to proceed to claims

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       element: <Index />
     },
     {
-      path: "/itemDetails",
+      path: "/item/:id",
       element: <ItemDetails />
     },
     {
@@ -46,4 +46,4 @@ function App() {
 
 }
 
-export default App
+export default App;
