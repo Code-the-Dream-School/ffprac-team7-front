43e8038b42fec02cc7406c4eafe5beae392a7517
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-15 11:22:40 -0400

    getting close but need help

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,11 +7,17 @@ function LoginPage() {
     return(
         <div>
             <Header />
+                <h2 className="greetingContainer" >Welcome to StuffFindr</h2>
             <div className="LoginPageContainer">
-                <h2>Welcome to StuffFindr</h2>
-                <input type='text' placeholder='username' disabled />
-                <input type='password' placeholder='password' disabled />
-                <button disabled>Submit</button>
+                <h2>Log In</h2>
+                <div className="inputContainer">
+                    <label className="fieldLabel">UserName</label>
+                        <input className="inputField" type='text'  disabled />
+                    <label className="fieldLabel">Password</label>
+                        <input className="inputField" type='password'  disabled />
+                </div>
+                <p>Don't have an account? <Link to="/SignUpPage">Sign up</Link></p>
+                <button className="logInBtn" disabled>Log In</button>
             </div>
         </div>
     );
