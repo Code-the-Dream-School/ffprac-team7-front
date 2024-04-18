86c5c2c5d191e9cb6bf098c1d132d1e01f58cc72
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-04-17 23:19:04 -0400

    complete UI/Basic functionality

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom"
 import Header from "../shared/header";
 import "../styles/LoginPage.css";
+import { Icon } from 'react-icons-kit';
+import { eyeOff } from 'react-icons-kit/feather/eyeOff';
+import { eye } from 'react-icons-kit/feather/eye';
 
+function validatePassword(password) {
+    const valid = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&*()_+])[A-Za-z\d@#$%^&*()_+]{8,}$/;
+    return valid.test(password);
+}
 function LoginPage() {
+
+    const [password, setPassword] = useState("");
+    const [type, setType] = useState('password');
+    const [icon, setIcon] = useState(eyeOff);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+    const handlePasswordChange = (e) => {
+        const newPassword = e.target.value;
+        setPassword(newPassword);
+        setIsPasswordValid(validatePassword(newPassword));
+    };
+
+    const handleToggle = () => {
+        if (type === 'password') {
+            setIcon(eye);
+            setType('text')
+        } else {
+            setIcon(eyeOff)
+            setType('password')
+        }
+    }
+
     return (
         <div>
             <Header />
@@ -18,8 +47,21 @@ function LoginPage() {
                 <div className="passwordContainer">
                     <label className="fieldLabel" htmlFor="password">Password</label>
                     <div className='passwordInputContainer'>
-                        <input className="inputField" type='password' disabled />
-                        {/* password eye icon here? */}
+                        <input
+                            className={`inputField ${isPasswordValid ? '' : 'invalid'}`}
+                            type={type}
+                            name="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={handlePasswordChange}
+                            autoComplete="current-password"
+                        />
+                        {!isPasswordValid && (
+                            <p className="passwordError">Password must contain at least one capital and lowercase letter, one number, one symbol, and must be at least 8 characters long.</p>
+                        )}
+                        <span className="eye" onClick={handleToggle}>
+                            <Icon className="eyeIcon" icon={icon} size={25} />
+                        </span>
                     </div>
                 </div>
                 <p className="signUpBlurb">Don't have an account? <Link to="/SignUpPage">Sign up</Link></p>
