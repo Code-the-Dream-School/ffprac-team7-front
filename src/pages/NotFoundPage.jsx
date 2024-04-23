39aac4bb58a25ca1e39b7d6d031100f8d1a1fe7a
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-04-22 21:10:47 -0400

    add Oops page not found

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NotFoundImage from "../assets/404.svg";
 import { Link } from 'react-router-dom';
-
+// Do some research to see how to make react-router make all error pages navigate to the not found page.
 const NotFoundPage = () => {
     return (
         <div className="notFoundContainer">
@@ -10,6 +10,7 @@ const NotFoundPage = () => {
                 alt="404 Page Not found"
                 className="pageNotFoundImg"
             />
+            <p>Oops!... Page Not Found.</p>
             <Link to="/">Back to Homepage</Link>
         </div>
     )
