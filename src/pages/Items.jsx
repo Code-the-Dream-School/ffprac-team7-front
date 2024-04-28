5cd22d82919826a5847fb44ad10b86f88003301c
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-27 22:40:32 -0400

    not crashed

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -11,85 +11,77 @@ const token =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NjJkNzAzZWJ
 const encodedToken = encodeURIComponent(token);
 
 
-  const Items = () => {
-    const [items, setItems] = useState([]);
-    const [filter, setFilter] = useState("");
-    const [error, setError] = useState(null);
-   
-    useEffect(() => {
-      fetchItems();
-    }, []);
+const Items = () => {
+  const [items, setItems] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [error, setError] = useState(null);
+ 
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-
-    const fetchItems = async () => {
-      try {
-        const response = await fetch('http://localhost:8000/api/v1/items', {
-         
-          headers: {
-              Authorization:`Bearer ${ encodedToken }`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error('Failed to fetch');
-        }
-        const data = await response.json();
-        console.log(data);
-        setItems(data);
-      } catch (error) {
-        setError(error.message);
-        toast.error('Failed to fetch items');
-        console.log(error.message)
+  const fetchItems = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/api/v1/items', {
+        headers: {
+            Authorization:`Bearer ${ encodedToken }`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Failed to fetch');
       }
-    };
+      const data = await response.json();
+      console.log(data);
+      setItems(data);
+    } catch (error) {
+      setError(error.message);
+      toast.error('Failed to fetch items');
+      console.log(error.message)
+    }
+  };
 
+  const handleFilterChange = (event) => {
+    const { value } = event.target;
+    setFilter(value);
+  };
 
+  const filteredItems = items.length > 0 ? items.filter((item) => {
+    const titleMatch = item.title.toLowerCase().includes(filter.toLowerCase());
+    const dateMatch = item.dateLostFound.includes(filter);
+    return titleMatch || dateMatch;
+  }) : [];
 
-    const handleFilterChange = (event) => {
-      const { value } = event.target;
-      setFilter(value);
-    };
-   
-    const filteredItems = items.filter((item) => {
-      const titleMatch = item.title.toLowerCase().includes(filter.toLowerCase());
-      const dateMatch = item.dateLostFound.includes(filter);
-      return titleMatch || dateMatch;
-    });
-   
-
-   
-    return (
-      <div>
-        <Header pageTitle="Reported Items" />
-        <div className="searchContainer">
-          <input
-            className="searchField"
-            type="text"
-            placeholder="Search: title or date"
-            value={filter}
-            onChange={handleFilterChange}
-            />
-        </div>
-        {filteredItems.length === 0 ? (
-          <div className="noResultsMessage">No Results Found</div>
-         ):(
+  return (
+    <div>
+      <Header pageTitle="Reported Items" />
+      <div className="searchContainer">
+        <input
+          className="searchField"
+          type="text"
+          placeholder="Search: title or date"
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+      {filteredItems.length === 0 ? (
+        <div className="noResultsMessage">No Results Found</div>
+      ) : (
         <div className="reportedItemsContainer">
           {filteredItems.map((item) => (
-            // <Link key={item.id} to={`/item/${item.id}`}>
             <ItemCard
-            key={item._id}
-            title={item.title}
-            description={item.description}
-            location={item.location}
-            lost={item.lost}
-            dateReported={item.dateReported}
-          />
-            // </Link>
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              location={item.location}
+              lost={item.lost}
+              dateReported={item.dateReported}
+            />
           ))}
         </div>
       )}
       <ToastContainer className='itemsToast'/>
-      </div>
-    );
-  };
- 
-  export default Items;
+    </div>
+  );
+};
+
+export default Items;
