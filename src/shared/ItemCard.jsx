ab84f9865be43313175e1ff51e446661c5a3c3c6
Tristan White <113216091+up2code3@users.noreply.github.com>
2024-04-29 20:40:31 -0400

    image fix

--- src/shared/ItemCard.jsx
+++ src/shared/ItemCard.jsx
@@ -9,7 +9,7 @@ const ItemCard = ({images, title, dateLostFound, type,description, location }) =
 return (
 <div className="reportedItemsContainer" >
    <div className="ItemCard">
-    <img className="itemPhoto" src={images || placeHolder}/>
+    <img className="itemPhoto" src={ placeHolder}/>
     <h3 className="itemTitle">{title}</h3>
     <p>{description}</p>
     <p className="dateLostFound">Reported: {dateLostFound}</p>
